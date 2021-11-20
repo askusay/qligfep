254e0451980239f3f7b3d0870fe99e0dae51c7b8
askusay <59225690+askusay@users.noreply.github.com>
2021-11-21 00:09:52 +1100

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,14 +6,15 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 FF_DIR = os.path.join(ROOT_DIR, "FF")
 INPUT_DIR = os.path.join(ROOT_DIR, "INPUTS")
 # Dicionary of locations of Q executables
-Q_DIR = {'CSB':'/home/jespers/software/q6/bin/',
-         'LOCAL':'/home/jespers/software/q6/bin/'
+Q_DIR = {'CSB':'/project/RDS-FMH-BalleMD-RW/Software/Q/q6/bin/',
+         'LOCAL':'/Software/q6/bin/'
          #'LOCAL':'/Users/willemjespers/Software/Q6/bin/'
+         # 'CSB':'~/Downloads/q6/bin/',
         }
 BIN = os.path.join(ROOT_DIR, "bin")
 #SCHROD_DIR = '/opt/schrodinger/suites2017-3/'
 #SCHROD_DIR = '/home/apps/schrodinger2017/'
-SCHROD_DIR = '/rmvw9010_software_home/schrodinger/latest/'
+SCHROD_DIR = '/home/Software/schrodinger_updated/'
 
 # CLUSTER INPUTS. To add your own cluster, use the same input as below
 CSB = {'NODES'        : '1',
@@ -25,6 +26,17 @@ CSB = {'NODES'        : '1',
        'QFEP'         : Q_DIR['CSB'] + 'qfep',
        'QCALC'        : Q_DIR['CSB'] + 'qcalc'
       }
+
+ARTEMIS_24 = {'MAINDIR'      : '/project/RDS-FMH-BalleMD-RW/Software/Q/q6/',
+         'NODES'        : '1',
+         'NTASKS'       : '24',
+         'TIME'         : '0-3:00:00',  # d-hh:mm:ss
+         'MODULES'      : 'module load gcc/4.9.3\n module load openmpi-gcc/1.8.4',
+         'QDYN'         : '/project/RDS-FMH-BalleMD-RW/Software/Q/q6/bin/qdynp',
+         'QPREP'        : Q_DIR['CSB'] + 'qprep',
+         'QFEP'         : '/project/RDS-FMH-BalleMD-RW/Software/Q/q6/bin/qfep',
+         'QCALC'        : '/project/RDS-FMH-BalleMD-RW/Software/Q/q6/bin/qcalc'
+        }
 
 ALICE = {'MAINDIR'      : '/home/jespersw/software/q6/',
          'NODES'        : '1',
